e70623289332bc82b86aa6212e5fff7931fba15c
YRAL375 <vadim.voytkus@solbeg.com>
2020-08-15 12:53:53 +0300

    Types

---
+++ types.ts
@@ -0,0 +1,15 @@
+const isLoading: boolean = true;
+
+const int: number = 30;
+const float: number = 4.3;
+const num: number = 4e39;
+
+const message: string = 'Message!';
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13];
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13];
+
+const stringArray: string[] = ['hello', 'typescript'];
+
+//Tuple
+const contact: [string, number] = ['Vadzim', 23455];
